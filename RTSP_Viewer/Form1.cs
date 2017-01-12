3659d40edce2a8441b7cc124bb028e3df8ef0a88
thephez <thephez@users.noreply.github.com>
2017-01-12 12:04:19 -0500

    Update to use VLC install folder lib/plugins  - Exits with error dialog if VLC install not found

--- RTSP_Viewer/Form1.cs
+++ RTSP_Viewer/Form1.cs
@@ -22,41 +22,11 @@ namespace RTSP_Viewer
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             myVlcControl.Stop();
-            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).BeginInit();
-
-            var currentDirectory = new FileInfo(Assembly.GetEntryAssembly().Location).DirectoryName;
-            DirectoryInfo VlCLibDirectory = new DirectoryInfo(currentDirectory);
-            myVlcControl.VlcLibDirectory = VlCLibDirectory;
-
-            // Standalone player
-            //Vlc.DotNet.Core.VlcMediaPlayer mp = new Vlc.DotNet.Core.VlcMediaPlayer(VlCLibDirectory);
-            //mp.SetMedia(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
-            //mp.Play();
-
-            myVlcControl.Location = new Point(0, 0);
-            myVlcControl.Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom);
-            myVlcControl.Size = new Size(800, 600);
-            myVlcControl.Name = "Vlc1";
-            myVlcControl.Rate = (float)0.0;
-            myVlcControl.BackColor = Color.Gray;
-
-            myVlcControl.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            myVlcControl.TabIndex = 0;
-            myVlcControl.Text = "vlcRincewindControl1";
-            
-            // Events
-            myVlcControl.Playing += OnVlcPlaying;
-            myVlcControl.EncounteredError += MyVlcControl_EncounteredError;
-            myVlcControl.LengthChanged += MyVlcControl_LengthChanged;
-
-            // Had to add this line to make work
-            ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).EndInit();
+            SetupVlc();
 
             Button playBtn = new Button();
             playBtn.Text = "Play";
@@ -71,7 +41,6 @@ namespace RTSP_Viewer
             stopBtn.Anchor = (AnchorStyles.Left | AnchorStyles.Bottom);
             stopBtn.Click += StopBtn_Click;
             this.Controls.Add(stopBtn);
-
             
             uri.Text = "rtsp://127.0.0.1:554/rtsp_tunnel?h26x=4&line=1&inst=1";
             uri.Location = new Point(10, myVlcControl.Bottom + 5 + playBtn.Height + 5);
@@ -81,13 +50,65 @@ namespace RTSP_Viewer
 
             Debug.Print(myVlcControl.VlcLibDirectory.ToString());
 
-            //myVlcControl.Play();
             this.Controls.Add(myVlcControl);
+        }
+
+        private void SetupVlc()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).BeginInit();
+
+            myVlcControl.VlcLibDirectory = GetVlcLibLocation();
+            myVlcControl.VlcMediaplayerOptions = new string[] { "--video-filter=deinterlace" };
+            // Standalone player
+            //Vlc.DotNet.Core.VlcMediaPlayer mp = new Vlc.DotNet.Core.VlcMediaPlayer(VlCLibDirectory);
+            //mp.SetMedia(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
+            //mp.Play();
+
+            myVlcControl.Location = new Point(0, 0);
+            myVlcControl.Anchor = (AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom);
+            myVlcControl.Size = new Size(800, 600);
+            myVlcControl.Name = "Vlc1";
+            myVlcControl.Rate = (float)0.0;
+            myVlcControl.BackColor = Color.Gray;
+
+            myVlcControl.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            myVlcControl.TabIndex = 0;
+
+            // Events
+            myVlcControl.Playing += OnVlcPlaying;
+            myVlcControl.EncounteredError += MyVlcControl_EncounteredError;
+            myVlcControl.LengthChanged += MyVlcControl_LengthChanged;
+
+            // Had to add this line to make work
+            ((System.ComponentModel.ISupportInitialize)(this.myVlcControl)).EndInit();
+        }
+
+        /// <summary>
+        /// Get the VLC install location (to reference the required DLLs and plugin folder)
+        /// </summary>
+        /// <returns>VLC install directory</returns>
+        private DirectoryInfo GetVlcLibLocation()
+        {
+            // Check both potential normal install locations
+            DirectoryInfo vlcLibDirectory = new DirectoryInfo("C:\\Program Files\\VideoLAN\\VLC");
+            DirectoryInfo vlcLibDirectoryX = new DirectoryInfo("C:\\Program Files (x86)\\VideoLAN\\VLCs");
+
+            if (vlcLibDirectory.Exists)
+            {
+                return vlcLibDirectory;
+            } else if (vlcLibDirectoryX.Exists)
+            {
+                return vlcLibDirectoryX;
+            } else
+            {
+                MessageBox.Show(this, "VLC install folder not found.  Libraries cannot be loaded.\nApplication will now close.", "Libraries not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                throw new DirectoryNotFoundException("VLC install folder not found.  Libraries cannot be loaded.");
+            }
         }
 
         private void StopBtn_Click(object sender, EventArgs e)
         {
-            
             myVlcControl.Stop();
         }
 
@@ -105,7 +126,7 @@ namespace RTSP_Viewer
         {
             Debug.Print(myVlcControl.State.ToString());
             //myVlcControl.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
-            myVlcControl.Play(new Uri(this.uri.Text));
+            myVlcControl.Play(new Uri(this.uri.Text), "");
             Debug.Print(myVlcControl.State.ToString());
         }
 
@@ -125,23 +146,15 @@ namespace RTSP_Viewer
             {
                 if (mediaInformation.Type == Vlc.DotNet.Core.Interops.Signatures.MediaTrackTypes.Audio)
                 {
+                    Console.WriteLine(string.Format("Audio info - Codec: {0}, Channels: {1}, Rate: {2}", mediaInformation.CodecName, mediaInformation.Audio.Channels, mediaInformation.Audio.Rate));
                     //myLblAudioCodec.InvokeIfRequired(l => l.Text += mediaInformation.CodecName);
-                    //myLblAudioChannels.InvokeIfRequired(l => l.Text += mediaInformation.Audio.Channels);
-                    //myLblAudioRate.InvokeIfRequired(l => l.Text += mediaInformation.Audio.Rate);
                 }
-            //    else if (mediaInformation.Type == Vlc.DotNet.Core.Interops.Signatures.MediaTrackTypes.Video)
-            //    {
-            //        //myLblVideoCodec.InvokeIfRequired(l => l.Text += mediaInformation.CodecName);
-            //        //myLblVideoHeight.InvokeIfRequired(l => l.Text += mediaInformation.Video.Height);
-            //        //myLblVideoWidth.InvokeIfRequired(l => l.Text += mediaInformation.Video.Width);
-            //    }
+                else if (mediaInformation.Type == Vlc.DotNet.Core.Interops.Signatures.MediaTrackTypes.Video)
+                {
+                    Console.WriteLine(string.Format("Video info - Codec: {0}, Height: {1}, Width: {2}", mediaInformation.CodecName, mediaInformation.Video.Height, mediaInformation.Video.Width));
+                    //        //myLblVideoCodec.InvokeIfRequired(l => l.Text += mediaInformation.CodecName);
+                }
             }
-
-            //myCbxAspectRatio.InvokeIfRequired(c =>
-            //{
-            //    c.Text = myVlcControl.Video.AspectRatio;
-            //    c.Enabled = true;
-            //});
         }
     }
 }
