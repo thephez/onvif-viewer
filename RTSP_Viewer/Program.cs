882215eeff9b9ad3729b0a3bd5e94b411cd4f4e3
thephez <thephez@users.noreply.github.com>
2017-02-08 11:17:00 -0500

    Added Unhandled Exception catch / message box

--- RTSP_Viewer/Program.cs
+++ RTSP_Viewer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using log4net;
+using System.Threading;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
@@ -17,12 +18,26 @@ namespace RTSP_Viewer
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Viewer form = new Viewer();
             form.Size = new System.Drawing.Size(800, 500);
             Application.Run(form);
             //Application.Run(new Form1());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(string.Format("Unhandled exception.  Application may not work properly.\n\nException: {0}\n\nInner Exception: {1}", e.Exception.Message, e.Exception.InnerException), "Unhandled Thread Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception).Message, "Unhandled UI Exception");
         }
     }
 }
